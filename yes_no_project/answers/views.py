41cffe47dab921cb33fa2445b6d46eaecb909e76
under <un@prontomarketing.com>
2020-01-09 13:11:16 +0700

    Add answer views :syringe:

--- yes_no_project/answers/views.py
+++ yes_no_project/answers/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import random
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views import View
+
+from answers.models import Answer
+
 
 # Create your views here.
+def answer_views(requests):
+    answers = Answer.objects.all()
+    answer = random.choice(answers)
+
+    return render(requests, 'answer.html', context={'answer': answer})
+
+
+def create_answer_views(request):
+    if request.method == 'GET':
+        return render(request, 'create_answer.html')
+
+    if request.method == 'POST':
+        if request.POST['text'] and request.POST['image']:
+            Answer.objects.create(
+                text=request.POST['text'],
+                image=request.POST['image']
+            )
+        return redirect('answer_views_url')
+
+
+class CreateAnswerView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'create_answer.html')
+
+    def post(self, request, *args, **kwargs):
+        if request.POST['text'] and request.POST['image']:
+            Answer.objects.create(
+                text=request.POST['text'],
+                image=request.POST['image']
+            )
+            return redirect('answer_views_url')
